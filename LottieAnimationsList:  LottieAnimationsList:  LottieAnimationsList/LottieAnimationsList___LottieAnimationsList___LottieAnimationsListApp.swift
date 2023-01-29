161258a1012e5dae9ebab5ab8b95ecdc1ba0b4ee
Morgan <Mharrisstoertz@gmail.com>
2023-01-29 15:15:19 -0500

    Initial Commit

---
+++ LottieAnimationsList:  LottieAnimationsList:  LottieAnimationsList/LottieAnimationsList___LottieAnimationsList___LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsList___LottieAnimationsList___LottieAnimationsListApp.swift
+//  LottieAnimationsList:  LottieAnimationsList:  LottieAnimationsList
+//
+//  Created by Morgan Harris-Stoertz on 2023-01-29.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsList___LottieAnimationsList___LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
