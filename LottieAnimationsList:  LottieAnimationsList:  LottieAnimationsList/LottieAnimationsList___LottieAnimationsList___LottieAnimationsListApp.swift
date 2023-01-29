fe8a7b32c8aab73b9e2b1f550758b3976efa35fb
Morgan <Mharrisstoertz@gmail.com>
2023-01-29 15:19:01 -0500

    Organized project and created a list view to show interesting animations.

--- LottieAnimationsList:  LottieAnimationsList:  LottieAnimationsList/LottieAnimationsList___LottieAnimationsList___LottieAnimationsListApp.swift
+++ LottieAnimationsList:  LottieAnimationsList:  LottieAnimationsList/LottieAnimationsList___LottieAnimationsList___LottieAnimationsListApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LottieAnimationsList___LottieAnimationsList___LottieAnimationsListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AnimationsListView()
         }
     }
 }
---
+++ LottieAnimationsList:  LottieAnimationsList:  LottieAnimationsList/Views/AnimationsListView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimationsListView.swift
+//  LottieAnimationsList:  LottieAnimationsList:  LottieAnimationsList
+//
+//  Created by Morgan Harris-Stoertz on 2023-01-29.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimationsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationsListView()
+    }
+}
